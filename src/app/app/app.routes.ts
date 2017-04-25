ffbd50438a91dfbfe089eed5dc1c68cb6df46881
anuradharg <anuradharg@users.noreply.github.com>
2017-04-26 00:55:26 +0530

    Delete app.routes.ts

--- src/app/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { DirectoryComponent } from "./directory/directory.component"
-import { HomeComponent }      from "./home/home.component"
-
-export const APP_ROUTES = [
-  {path: 'directory', component: DirectoryComponent},
-  {path: '', component: HomeComponent}
-];
