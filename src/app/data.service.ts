188799eaff30e059dc785ff482b504739962ccf0
anuradharg <anuradharg@users.noreply.github.com>
2017-04-26 19:57:14 +0530

    Lesson-25
    Connected to Firebase. Changes applicable only in data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -12,10 +12,11 @@ export class DataService {
     /*
     Method-1
     get() returns an Observable, which should be subscribed to,
-    in order to access the data- this should be done in the component which invokes fetchData();
+    in order to access the data - this should be done in the component which invokes fetchData();
+    
     The output of get() is assigned to another Observable - the map() to return data in JSON format
     */
-    return this.http.get('/ninjas.json').map(
+    return this.http.get('https://angular2-6d9aa.firebaseio.com/.json').map(
       (res) => res.json()
     );
 
